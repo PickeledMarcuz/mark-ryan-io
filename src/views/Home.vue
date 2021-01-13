b446d4c563559d8f201946922d566a4086dfe7e0
PickeledMarcuz <mark.ryan25@gmail.com>
2021-01-13 16:21:21 +0200

    Refactor Home to skeleton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div
+      class="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl"
+    >
+      <p
+        class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
+      >
+        -- Content goes here --
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
