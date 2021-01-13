007f00efffb28426b03f2e6584a14492bb167a0d
PickeledMarcuz <mark.ryan25@gmail.com>
2021-01-13 16:20:21 +0200

    General skeleton link tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,147 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header class="text-gray-700 bg-white border-t border-b body-font">
+      <div
+        class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row "
+      >
+        <a
+          class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0"
+        >
+          <img src="#" alt="" />
+        </a>
+        <nav
+          class="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto"
+        >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Pricing</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Contact</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Services</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Now</a
+          >
+        </nav>
+        <button
+          class="items-center px-8 py-3 mt-4 font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-800 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
+        >
+          Button
+        </button>
+      </div>
+    </header>
+
+    <!-- Content from Vue application -->
     <router-view />
+
+    <footer class="mt-6 text-gray-700 bg-white border-t body-font">
+      <div class="border-t border-gray-200">
+        <div
+          class="container flex flex-col flex-wrap items-center p-5 mx-auto "
+        >
+          <div class="inline-flex items-center py-20">
+            <span
+              class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start"
+            >
+              <a class="text-blueGray-300 hover:text-blueGray-400">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-16 h-16"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+                  ></path>
+                </svg>
+              </a>
+              <a class="mx-5 text-blueGray-300 hover:text-blueGray-400">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-16 h-16"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+                  ></path>
+                </svg>
+              </a>
+              <a class=" text-blueGray-300 hover:text-blueGray-400">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-16 h-16"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path
+                    d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
+                  ></path>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+        <div class="bg-blueGray-900">
+          <div
+            class="container flex flex-col flex-wrap px-5 py-6 mx-auto lg:px-20 sm:flex-row"
+          >
+            <div
+              class="flex flex-wrap items-center justify-center mx-auto text-base"
+            >
+              <a
+                href="#"
+                class="justify-center mr-5 text-sm text-center text-blueGray-200 hover:text-blue-700"
+              >
+                Privacy Policy</a
+              >
+              <a
+                href="#"
+                class="justify-center mr-5 text-sm text-center text-blueGray-200 hover:text-blue-700"
+              >
+                License</a
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex flex-wrap items-center justify-center px-5 py-6 mx-auto text-base bg-blueGray-900 "
+        >
+          <div
+            class="flex flex-wrap items-center justify-center mx-auto text-base"
+          >
+            <p class="mr-5 text-sm text-center text-gray-200">
+              © Your Company — 2020
+            </p>
+
+            <a
+              href="#"
+              class="justify-center mr-5 text-sm text-center text-blueGray-200 hover:text-blue-700"
+            >
+              All rights Reserves</a
+            >
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -15,18 +152,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
