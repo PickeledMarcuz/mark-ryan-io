eb4b1ee94ee67b556c64b0fd33200904cb928370
PickeledMarcuz <mark.ryan25@gmail.com>
2021-01-14 10:45:19 +0200

    Routerlink for nav

--- src/App.vue
+++ src/App.vue
@@ -4,45 +4,65 @@
       <div
         class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row "
       >
-        <a
+        <router-link to="/"
           class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0"
         >
-          <img src="#" alt="" />
-        </a>
+          <img
+            class="h-12 w-12 profile-pic bg-white rounded-xl"
+            src="@/assets/Logo.png"
+            alt="brand logo"
+          />
+        </router-link>
         <nav
           class="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto"
         >
-          <a
-            href="#"
-            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-            >Pricing</a
+          <router-link
+            to="/about"
+            class="mr-5 text-sm font-semibold text-gray-600
+            hover:text-gray-800"
           >
-          <a
-            href="#"
-            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-            >Contact</a
+            About</router-link
           >
-          <a
-            href="#"
-            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-            >Services</a
+          <router-link
+            to="/contact"
+            class="mr-5 text-sm font-semibold text-gray-600
+            hover:text-gray-800"
           >
-          <a
-            href="#"
-            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-            >Now</a
+            Contact</router-link
+          >
+          <router-link
+            to="/services"
+            class="mr-5 text-sm font-semibold text-gray-600
+            hover:text-gray-800"
+          >
+            Services</router-link
+          >
+          <router-link
+            to="/blog"
+            class="mr-5 text-sm font-semibold text-gray-600
+            hover:text-gray-800"
+          >
+            Blog</router-link
           >
         </nav>
         <button
           class="items-center px-8 py-3 mt-4 font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-800 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
         >
-          Button
+          Log In / Register
         </button>
       </div>
     </header>
 
     <!-- Content from Vue application -->
-    <router-view />
+    <transition
+      name="fade"
+      mode="out-in"
+      @beforeLeave="beforeLeave"
+      @enter="enter"
+      @afterEnter="afterEnter"
+    >
+      <router-view />
+    </transition>
 
     <footer class="mt-6 text-gray-700 bg-white border-t body-font">
       <div class="border-t border-gray-200">
@@ -152,5 +172,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 300ms;
+  transition-property: opacity;
+  transition-timing-function: ease-out;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
