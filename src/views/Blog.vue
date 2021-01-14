33d4e503da9d6cc149154af2ef6912ef6f590fda
PickeledMarcuz <mark.ryan25@gmail.com>
2021-01-14 13:02:18 +0200

    Add flesh for blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="blog">
-    <div
-      class="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl"
-    >
-      <p
-        class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
-      >
-        -- Content goes here --
-      </p>
-    </div>
+    <section class="text-gray-700 body-font">
+      <div class="container px-8 mx-auto py-36 lg:px-4">
+        <div class="flex flex-col w-full mb-12 text-left lg:text-center">
+          <h2
+            class="mb-1 text-xs font-semibold tracking-widest text-blue-600 uppercase title-font"
+          >
+            a great header right here
+          </h2>
+          <h1
+            class="mb-6 text-2xl font-semibold tracking-tighter text-blue-800 sm:text-6xl title-font"
+          >
+            A Long headline
+            <br class="" />
+            to convey your users.
+          </h1>
+          <p
+            class="mx-auto text-base font-medium leading-relaxed text-gray-700 lg:w-1/2"
+          >
+            Tailwind CSS templates with a wicked design. Professionally designed
+            and 100% responsive static templates for startups and personal
+            use.Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table. Franzen you probably
+            haven't heard of them man bun deep.
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
