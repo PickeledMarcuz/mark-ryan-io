793e64c0639c71fda98c610aa1da611bb14c0cab
PickeledMarcuz <mark.ryan25@gmail.com>
2021-01-14 10:55:40 +0200

    Added routes for pages with lazy load

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "Home", // No lazy load for home as we always want it to load first thing.
     component: Home
   },
   {
@@ -18,6 +18,33 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () =>
+      import(/* webpackChunkName: "contact" */ "../views/Contact.vue")
+  },
+  {
+    path: "/services",
+    name: "Services",
+    component: () =>
+      import(/* webpackChunkName: "services" */ "../views/Services.vue")
+  },
+  {
+    path: "/blog",
+    name: "Blog",
+    component: () => import(/* webpackChunkName: "about" */ "../views/Blog.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+    meta: {
+      requiresAuth: true,
+      hideNavigation: true
+    }
   }
 ];
 
