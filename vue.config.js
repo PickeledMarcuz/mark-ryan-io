43b9b32fcf2fe3cd1f0da600ba5648568e75440a
Mark Ryan <mark.ryan@futurice.com>
2021-10-17 19:50:07 +0300

    Vue config with chain settings

---
+++ vue.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  lintOnSave: process.env.NODE_ENV !== "production",
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  configureWebpack: {
+    // Add plugins here if needed
+    // plugins: [new MyAwesomeWebpackPlugin()]
+  },
+
+  // productions or development setup
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  },
+
+  // Images imports
+  chainWebpack: config => {
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap(options => Object.assign(options, { limit: 10240 }));
+  },
+
+  // CSS imports
+  chainWebpack: config => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type => {
+      addStyleResource(config.module.rule("stylus").oneOf(type));
+    });
+  },
+
+  // SVG imports
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+
+    // clear all existing loaders.
+    // if you don't do this, the loader below will be appended to
+    // existing loaders of the rule.
+    svgRule.uses.clear();
+
+    // add replacement loader(s)
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  }
+};
